1ebc169bb89b0d18914584e55fcc3296b93989ee
Yannick Moris <yanndevpro@gmail.com>
2023-02-09 14:12:39 +0100

    animation navbar

--- app.js
+++ app.js
@@ -6,20 +6,34 @@ const fermetureModale = document.querySelector('.fermeture-modale');
 const imgIndex = document.querySelector('.bloc-modale img');
 
 // je boucle mes modales
-btns.forEach(btn => {
-    btn.addEventListener('click', (e) => {
+if(window.matchMedia("(min-width: 850px)").matches) {
+    btns.forEach(btn => {
+        btn.addEventListener('click', (e) => {
 
-        imgIndex.src = `ressources/img${e.target.getAttribute('data-index')}-equip.jpg`;
-        modaleEquipement.classList.add('active-modale');
+            imgIndex.src = `ressources/img${e.target.getAttribute('data-index')}-equip.jpg`;
+            modaleEquipement.classList.add('active-modale');
+        })
     })
-})
 
-// Fermutre avec le bouton
-fermetureModale.addEventListener('click', (e) => {
-    modaleEquipement.classList.remove('active-modale');
-})
+    // Fermutre avec le bouton
+    // fermetureModale.addEventListener('click', (e) => {
+    //     modaleEquipement.classList.remove('active-modale');
+    // })
 
-// fermeture en cliquant sur l'image
-modaleEquipement.addEventListener('click', (e) => {
-    modaleEquipement.classList.remove('active-modale');
+    // fermeture en cliquant sur l'image
+    modaleEquipement.addEventListener('click', (e) => {
+        modaleEquipement.classList.remove('active-modale');
+    })
+}
+
+// Anim Navbar
+
+const nav = document.querySelector('nav');
+
+window.addEventListener('scroll', () => {
+    if(window.scrollY > 30) {
+        nav.classList.add('anim-nav');
+    }else {
+        nav.classList.remove('anim-nav');
+    }
 })
